24d69683d0fd33b1303e75db249dbbaea470f341
Mindaugas <darbas.mindaugas@gmail.com>
2021-09-06 11:14:31 +0300

    Adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,32 @@
 import { Component } from '@angular/core';
 
+// app.component.ts
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
   template: `
-    <xyz></xyz>
-    <p>Hello world</p>
-    <button>Hi ---</button>
-    <pre>[ vardas: "Jonas" ]</pre>
+    <app-header></app-header>
+    <div class="container">
+      <p>Hello world</p>
+      <button>Hi ---</button>
+      <pre>[ vardas: "Jonas" ]</pre>
 
-    <!-- (String) Interpolation -->
-    <p>{{ title }}</p>
-    <p>{{ subtitle }}</p>
-    <p>{{ subtitle + ' ' + title }}</p>
-    <p>2 + 2 = {{ 2 + 2 }}</p>
-    <p>firstNumber + secondNumber = {{ firstNumber + secondNumber }}</p>
+      <!-- (String) Interpolation -->
+      <p>{{ title }}</p>
+      <p>{{ subtitle }}</p>
+      <p>{{ subtitle + ' ' + title }}</p>
+      <p>2 + 2 = {{ 2 + 2 }}</p>
+      <p>firstNumber + secondNumber = {{ firstNumber + secondNumber }}</p>
 
-    <hr />
-    <p>Example with incrementation button</p>
-    <p>{{ counter }}</p>
-    <!-- <button (click)="counter = counter + 1">Click me!</button> -->
-    <button (click)="clickHandler()">Click me!</button>
+      <hr />
+      <p>Example with incrementation button</p>
+      <p>{{ counter }}</p>
+      <!-- <button (click)="counter = counter + 1">Click me!</button> -->
+      <button class="btn btn-warning" (click)="clickHandler()">Click me!</button>
+    <div>
   `,
-  styles: ['p {color: salmon;} ', ''],
+  styles: ['p {color: salmon;} ', 'button { background-color: white; }'],
 })
 export class AppComponent {
   title = 'FirstBitAngularApp';
