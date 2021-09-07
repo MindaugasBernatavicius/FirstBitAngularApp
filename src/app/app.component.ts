bd24061b8e470e6a268afe2b61b60e1f92753085
Mindaugas <darbas.mindaugas@gmail.com>
2021-09-07 11:16:55 +0300

    Adding data binding discussion code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
   template: `
     <app-header></app-header>
     <div class="container">
-      <p>Hello world</p>
+      <p style="background-color: red; width: {{ counter * 2 + counter2 }}px">Hello world</p>
       <button>Hi ---</button>
       <pre>[ vardas: "Jonas" ]</pre>
 
@@ -17,16 +17,23 @@ import { Component } from '@angular/core';
       <p>{{ subtitle }}</p>
       <p>{{ subtitle + ' ' + title }}</p>
       <p>2 + 2 = {{ 2 + 2 }}</p>
-      <p>firstNumber + secondNumber = {{ firstNumber + secondNumber }}</p>
+      <p>firstNumber + secondNumber = {{ firstNumber + getSecondNumber() }}</p>
 
       <hr />
       <p>Example with incrementation button</p>
       <p>{{ counter }}</p>
+      <p>{{ counter2 }}</p>
+      <p>{{ counter + counter2 }}</p>
       <!-- <button (click)="counter = counter + 1">Click me!</button> -->
       <button class="btn btn-warning" (click)="clickHandler()">Click me!</button>
+      <button class="btn btn-warning" (click)="clickHandler2()">Click me2!</button>
+
+      <a href="https://blog.mindaugas.cf">This will not be red!</a>
     <div>
   `,
-  styles: ['p {color: salmon;} ', 'button { background-color: white; }'],
+  styles: [
+    'p {color: salmon;} ',
+    'button { background-color: white; }'],
 })
 export class AppComponent {
   title = 'FirstBitAngularApp';
@@ -34,8 +41,19 @@ export class AppComponent {
   firstNumber = 5;
   secondNumber = 10;
 
+  getSecondNumber() {
+    return this.secondNumber;
+  }
+
   counter = 0;
   clickHandler() {
+    console.log(" >>> " + this.counter);
     this.counter++;
+  }
+
+  counter2 = 0;
+  clickHandler2() {
+    console.log(" >>> " + this.counter2);
+    --this.counter2;
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,14 +2,50 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  template: `
+    <!-- header.component.html -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="https://getbootstrap.com/docs/5.1/assets/brand/{{ headerFilname }}"
+            alt=""
+            width="30"
+            height="24"
+            class="d-inline-block align-text-top"
+          />
+          My Awesome App
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  `,
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  headerFilname = 'bootstrap-logo.svg';
+  // headerLogoUrl = 'https://getbootstrap.com/docs/5.1/assets/brand/' + this.headerFilname;
+  // 'https://getbootstrap.com/docs/5.1/assets/brand/' + 'bootstrap-logo.svg'
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
